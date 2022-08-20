c3e03e7057585f2353f240f250d476ecc9d0a8dd
Romuald Sandamalala <sandaromual@gmail.com>
2022-08-20 20:43:12 +0300

    Configure the project

---
+++ components/component.js
@@ -0,0 +1,14 @@
+
+
+/**
+ * C'est ici où on met les composants React
+ * 
+ * Exemple : 
+ */
+function Component(props) {
+    const [etat,setEtat] = useState(0);
+    
+    return <div>{etat}Hello world</div>
+}                                                                                                                                                                        
+
+export default Component;
